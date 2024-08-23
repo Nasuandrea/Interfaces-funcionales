6b4cb444079d24cd188d69a89b2bdfdb4e50ef7b
Nasuandrea <acollazocacho@gmail.com>
2024-08-23 10:56:40 +0200

    supplier, function

--- src/com/example/function/Main.java
+++ src/com/example/function/Main.java
@@ -1,7 +1,19 @@
 package com.example.function;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
+        //Function se utiliza para transformar o extraer datos: ej: pasamos un nombre y te devuelve la longitud de ese nombre. Metes texto sale un número
+        //Primer parametro: lo que pasamos, segundo: lo que devuelve
+        Function<String,Integer> extractLength = message -> message.length();
+        System.out.println("Longitud del texto: " + extractLength.apply("Andrea"));
+        System.out.println();
 
+        //BiFunction pasa dos parametros de entrada, uno de salida
+        //Cuando hay 0 o 2 parametros de entrada hay que poner paréntesis en la lambda, ej: (num1,num2) -> ...
+        BiFunction<Double, Double, Double>  divider = (num1,num2) -> num1/num2;
+        System.out.println("Resultado dividir: "+ divider.apply(6.0,3.0));
     }
 }
---
+++ src/com/example/suplier/Main.java
@@ -0,0 +1,17 @@
+package com.example.suplier;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        //Suplier proporciona un dato, no recibe nada, devuelve algo, le pasamos el tipo de dato que nos tiene que devolver
+        //Al recibir 0 parametros en la lambda poner () -> ...
+        Supplier<Double> generator = () -> Math.random();
+
+        System.out.println("Número aleatorio: " + generator.get());
+
+        Supplier<Double> generator2= () -> new Random().nextDouble(500+1);
+    }
+}

7277d86b6be485ade08b0785de40f2dde62bece4
Nasuandrea <acollazocacho@gmail.com>
2024-08-23 12:20:35 +0200

    metodos instancia

--- src/com/example/methodreference/instancemethods/Main.java
+++ src/com/example/methodreference/instancemethods/Main.java
@@ -4,12 +4,13 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        //Metodos instancia: cuando hay un objeto en vez de un método static
+        //Metodos instancia: cuando hay un objeto en vez de un método static - ya no es estatico el metodo de la clase,
+        //Accedemos a un método de un objeto no de una clase
         PositiveNumber positiveNumber = new PositiveNumber();
 
-        Predicate<Integer> predicate = num -> PositiveNumber.isPositive(num);
+        Predicate<Integer> predicate = num -> positiveNumber.isPositive(num);
 
-        Predicate<Integer> predicate1 = PositiveNumber::isPositive;
+        Predicate<Integer> predicate1 = positiveNumber::isPositive;
 
 
     }
@@ -17,7 +18,7 @@ public class Main {
 
 }
 class PositiveNumber{
-    public static boolean isPositive(int num){
+    public boolean isPositive(int num){
         return num >= 0 ;
     }
 }

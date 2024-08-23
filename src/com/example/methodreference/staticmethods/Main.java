d2dd974c199af2b230ad8e3a5ddb9f8ae2d1f843
Nasuandrea <acollazocacho@gmail.com>
2024-08-23 12:11:49 +0200

    metodos referencia

--- src/com/example/methodreference/staticmethods/Main.java
+++ src/com/example/methodreference/staticmethods/Main.java
@@ -10,13 +10,31 @@ public class Main {
 
         //Reemplazado por method reference "::" en este caso la clase PositiveNumber tiene un único metodo estatico isPositive
         Predicate<Integer> checkPositive1 = PositiveNumber::isPositive;
-        System.out.println("Comprobación número positivo con método de referencia: " + checkPositive1.test(10));
+        System.out.println("Comprobación número positivo con método de referencia de clase externa: " + checkPositive1.test(10));
+
+        Predicate<Integer> checkNegative = Main::isNegative;
+        System.out.println("Comprobación número negativo con método de referencia de Main: " + checkNegative.test(10));
+
+        BooleanCheck checkPositive2 = num -> num > 5;
+        System.out.println("comprobacion con interfaz funcional creada de num positivo: "+ checkPositive2.check(10));
+
+        BooleanCheck checkPositive3 = num -> PositiveNumber.isPositive(num);
+        System.out.println("comprobacion con interfaz creada de num positivo + lambda: "+ checkPositive3.check(10));
+
+        BooleanCheck checkPositive4 = PositiveNumber::isPositive;
+        System.out.println("comprobacion con interfaz creada de num positivo + method reference: "+ checkPositive4.check(10));
 
     }
+        public static boolean isNegative(int num){return num <= 0 ;}
 
     class PositiveNumber{
         public static boolean isPositive(int num){
-            return num > 0 ;
+            return num >= 0 ;
         }
+    }
+
+    @FunctionalInterface
+    interface BooleanCheck {
+        boolean check(int num);
     }
 }
